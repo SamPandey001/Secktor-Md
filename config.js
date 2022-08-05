d3bdfe9eb0656e733585937a4978d7a83db31acb
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-08-05 11:55:29 +0530

    © copyright by CitelVoid All rights reserved

---
+++ config.js
@@ -0,0 +1,48 @@
+const fs = require('fs')
+const chalk = require('chalk')
+//git test by Sam    huh
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = true //auto reading in gc (true to on, false to off)
+global.autoReadAll = false // auto reading in all pchat gc and status (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.available = true //auto available (true to on, false to off)
+//═══════[modification]════════\\
+let number = process.env.OWNER_NUMBER;
+if (!number) { number = "919628516236"; }
+global.owner = number.split(",");
+global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
+global.port = process.env.PORT || 5000
+global.botname = process.env.BOT_NAME || '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨'
+global.ownername = process.env.ownername || 'Citels' //owner name
+global.packname = 'Secktor' //sticker package name
+global.author = 'SamPandey001' //sticker author name
+global.sessionNamew = 'session.sam'
+global.sessionName = process.env.SESSION_ID || 'PUT-HERE'
+global.zerotwo = "https://github.com/SecktorBot/Secktor-MD" //ur gc link
+global.zerotwo2 = "https://github.com/SecktorBot/Secktor-MD" //ur gc link2
+global.websitex = "https://ZeroTwoMd.tech" //ur website
+global.prefa = process.env.PREFIX || ['-'] //prefix
+module.exports = {
+  nsfw_detect_ai : process.env.NSFW_DETECTION_AI || 'true',
+  pmpermit: process.env.PMPERMIT || "true",
+  pmpermit_mutetime: process.env.PMPERMIT_MUTETIME || "1800",
+  mongodb_url: process.env.MONGODB_URI || "Enter-MongoURI-HERE",
+  HEROKU: {
+        HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
+        API_KEY: process.env.HEROKU_API_KEY === undefined ? '1abfce1e-1bee-4334-9f6c-f4c1cb1cafab' : process.env.HEROKU_API_KEY,
+        APP_NAME: process.env.HEROKU_APP_NAME === undefined ? 'zeropgg' : process.env.HEROKU_APP_NAME
+    },
+  BRANCH: 'secktor',
+  VERSION: process.env.VERSION === undefined ? 'v.2.0' : process.env.VERSION,
+ LANG: process.env.THEME|| 'SECTOR',
+ SUPPORT: "120363040838753957@g.us",
+  WORKTYPE: process.env.WORKTYPE === undefined ? 'private' : process.env.WORKTYPE
+};
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+	require(file)
+})
---
+++ deploy.sh
@@ -0,0 +1,9 @@
+apt-get update && apt-get upgrade
+apt-get install git -y
+apt-get install nodejs -y
+apt-get install ffmpeg -y
+apt-get install imagemagick -y
+apt-get install yarn
+git clone https://github.com/Secktorbot/Secktor-Md
+cd Secktor-Md
+npm start
