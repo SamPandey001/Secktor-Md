3c8cccd92de478ee6b3dc1472f84d854f080c32e
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-09-05 19:47:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,26 @@
 const fs = require('fs')
 const chalk = require('chalk');
 const { existsSync } = require('fs')
-//═══════[modification]════════\\
+//═══════[Required Variables]════════\\
 let number = process.env.OWNER_NUMBER;
 if (!number) { number = "919628516236"; }
 global.owner = number.split(",");
 global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
 global.port= process.env.PORT || 5000
+global.email = 'sam@secktor.live'
+global.github = 'https://github.com/SamPandey001/Secktor-Md'
+global.location = 'Sultanpur IN'
 module.exports = {
   botname: process.env.BOT_NAME || '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨',
   ownername:process.env.ownername || 'Citels',
   sessionName: process.env.SESSION_ID || 'PUT-HERE',
   author: 'SamPandey001',
   packname: 'Secktor',
-  prefix: process.env.PREFIX || ['-'],
-  nsfw_detect_ai : process.env.NSFW_DETECTION_AI || 'true',
-  pmpermit: process.env.PMPERMIT || "true",
-  pmpermit_mutetime: process.env.PMPERMIT_MUTETIME || "1800",
-  mongodb_url: process.env.MONGODB_URI || "Enter-MongoURI-HERE",
+  autoreaction: process.env.AUTO_REACTION || 'off',
+  prefix: process.env.PREFIX || ['.'],
+  nsfw_detect_ai : process.env.NSFW_DETECTION_AI || 'false',
+  pmpermit: process.env.PMPERMIT || "false",
+  mongodb_url: process.env.MONGODB_URI || "Enter-Mongo-URI-HERE",
   HEROKU: {
         HEROKU: process.env.HEROKU || `true`,
         API_KEY: process.env.HEROKU_API_KEY === undefined ? '1abfce1e-1bee-4334-9f6c-f4c1cb1cafab' : process.env.HEROKU_API_KEY,
@@ -29,6 +32,8 @@ module.exports = {
  SUPPORT: "120363040838753957@g.us",
   WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
 };
+
+
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
