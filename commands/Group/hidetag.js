b95c1f5e7d26389e90a5d0076d43f7a1a1776935
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-09-27 07:27:31 +0530

    update hidetag.js

--- commands/Group/hidetag.js
+++ commands/Group/hidetag.js
@@ -4,12 +4,12 @@ module.exports = {
     name: 'hidetag',
     category: 'group',
     desc: 'Tags everyperson of group without mentioning their numbers.',
-    async exec(citel, Void) {
+    async exec(citel, Void,args) {
       if(!citel.isGroup) return citel.reply(tlang().title)
       const groupMetadata = citel.isGroup ? await Void.groupMetadata(citel.chat)
       .catch((e) => {}) : "";
       const participants = citel.isGroup ? await groupMetadata.participants : "";
-  if (!isBotAdmins) citel.reply(tlang().botAdmin);
+  if (!isAdmins) return citel.reply(tlang().admin);
         Void.sendMessage(citel.chat, {
           text: args.join(" ") ? args.join(" ") : "",
           mentions: participants.map((a) => a.id),
