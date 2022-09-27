a5273f65463d7ccc939ca0dce27cfe1f3ed14197
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-09-27 10:33:52 +0530

    Typo*

--- commands/Group/delete.js
+++ commands/Group/delete.js
@@ -5,7 +5,7 @@ module.exports = {
     category: 'group',
     desc: 'Deletes message of any user.',
     async exec(citel, Void,args) {
-        if (citel.isBaileys) {
+        if (citel.quoted.isBaileys) {
             const key = {
                 remoteJid: citel.chat,
                 fromMe: false,
@@ -15,7 +15,7 @@ module.exports = {
             await Void.sendMessage(citel.chat, { delete: key })
 
         }
-        if (!citel.isBaileys) {
+        if (!citel.quoted.isBaileys) {
         if (!isAdmins) return citel.reply('Only Admins are allowed to delete other persons message.')
         if (!citel.quoted) return citel.reply(`Please Quote any message. ${tlang().greet}`);
         let { chat, fromMe, id } = citel.quoted;
