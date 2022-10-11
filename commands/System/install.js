03b898045199077652b2a057292a2cc61d720b67
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-10-11 18:32:55 +0530

    Update install.js

--- commands/System/install.js
+++ commands/System/install.js
@@ -8,7 +8,7 @@ module.exports = {
    async exec(citel, Void,args,isCreator) {
     const { tlang } = require('../../lib/scraper')
     if(!isCreator) return citel.reply(tlang().owner)
- if (!args[0].includes('gist.')) return citel.reply("🔍 Please provide me a valid gist url.")
+ if (!args[0].includes('raw.')) return citel.reply("🔍 Please provide me a valid gist url.\nhttps://raw.githubusercontent.com/SamPandey001/Secktor-Md/main/commands/System/alive.js")
    await install(args[0])
 	citel.reply(`New plugin ${args[0]} added in db.`)
 }
