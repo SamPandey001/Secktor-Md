4ccc2c24a4b746e1e9bcb310ad7b947052c385c4
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-10-13 20:04:14 +0530

    Update remove.js

--- commands/System/remove.js
+++ commands/System/remove.js
@@ -1,12 +1,11 @@
-const { remove } = require('../../lib/scraper')
+const { remove,tlang } = require('../../lib')
 module.exports = {
    name: 'remove',
    category: 'owner',
    desc: 'removes external modules.',
    async exec(citel, Void,args,isCreator) {
-      const { tlang } = require('../../lib/scraper')
       if(!isCreator) return citel.reply(tlang().owner)
-    if (!args[0].includes('gist.')) return citel.reply("🔍 Please provide me a valid gist url.")
+    if (!args[0].includes('raw')) return citel.reply("🔍 Please provide me a valid gist url.")
     await remove(args[0])
            citel.reply(`Plugin ${args.join(" ")} deleted from mongodb.`)
 }
