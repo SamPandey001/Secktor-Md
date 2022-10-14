c31b6be6336d0ab0b803181d31c44a72c22d2758
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-10-14 18:31:37 +0530

    Update install.js

--- commands/System/install.js
+++ commands/System/install.js
@@ -1,15 +1,1 @@
-const { install } = require('../../lib')
-const axios = require('axios')
-
-module.exports = {
-   name: 'install',
-   category: 'owner',
-   desc: 'Installs external modules.',
-   async exec(citel, Void,args,isCreator) {
-    const { tlang } = require('../../lib/scraper')
-    if(!isCreator) return citel.reply(tlang().owner)
- if (!args[0].includes('raw.')) return citel.reply("🔍 Please provide me a valid gist url.\nhttps://raw.githubusercontent.com/SamPandey001/Secktor-Md/main/commands/System/alive.js")
-   await install(args[0])
-	citel.reply(`New plugin ${args[0]} added in db.`)
-}
-}
+function _0x4fe2(_0x400692,_0x1a93b6){const _0x37a123=_0x37a1();return _0x4fe2=function(_0x4fe256,_0x4588ae){_0x4fe256=_0x4fe256-0x105;let _0x5953c2=_0x37a123[_0x4fe256];return _0x5953c2;},_0x4fe2(_0x400692,_0x1a93b6);}const _0x19a02c=_0x4fe2;(function(_0x3aa1db,_0x228b52){const _0x298d1e=_0x4fe2,_0x3745da=_0x3aa1db();while(!![]){try{const _0x581e80=parseInt(_0x298d1e(0x11f))/0x1+-parseInt(_0x298d1e(0x108))/0x2*(parseInt(_0x298d1e(0x121))/0x3)+parseInt(_0x298d1e(0x10a))/0x4+-parseInt(_0x298d1e(0x11b))/0x5*(-parseInt(_0x298d1e(0x10b))/0x6)+-parseInt(_0x298d1e(0x115))/0x7*(parseInt(_0x298d1e(0x11c))/0x8)+parseInt(_0x298d1e(0x116))/0x9+-parseInt(_0x298d1e(0x112))/0xa;if(_0x581e80===_0x228b52)break;else _0x3745da['push'](_0x3745da['shift']());}catch(_0x1a5861){_0x3745da['push'](_0x3745da['shift']());}}}(_0x37a1,0xaf479));function _0x37a1(){const _0x972e7a=['Installs\x20external\x20modules.','7TxxbuV','7353225hivzkz','writeFileSync','stringify','get','replace','40LyYcnX','7755160DPeMfv','text','.js','169892AaZkTF','quoted','313359NYEhol','../../lib','Plugin\x20','reply','6KLFmGu','```','5530392DELJqY','832152bBVstQ','match','axios','owner','join','exports','\x20installed\x20in\x20Secktor','14783520MPECmN','utf8'];_0x37a1=function(){return _0x972e7a;};return _0x37a1();}const {install,isUrl,tlang}=require(_0x19a02c(0x105)),axios=require(_0x19a02c(0x10d));module[_0x19a02c(0x110)]={'name':'install','category':'owner','desc':_0x19a02c(0x114),async 'exec'(_0x20a1b9,_0x4fbcbf,_0x4f4615,_0x6f7577){const _0x209ec8=_0x19a02c;if(!_0x6f7577)return _0x20a1b9['reply'](tlang()[_0x209ec8(0x10e)]);let _0x5df422=_0x4f4615[_0x209ec8(0x10f)]('\x20')?_0x4f4615[_0x209ec8(0x10f)]('\x20'):_0x20a1b9['quoted']&&_0x20a1b9[_0x209ec8(0x120)][_0x209ec8(0x11d)]?_0x20a1b9['quoted'][_0x209ec8(0x11d)]:_0x20a1b9[_0x209ec8(0x11d)];for(let _0x1748d2 of isUrl(_0x5df422)){try{var _0x287801=new URL(_0x1748d2);}catch{_0x20a1b9[_0x209ec8(0x107)]('_Invalid\x20Url_');}let _0x3f3af3=require('fs'),{data:_0x5851ab}=await axios[_0x209ec8(0x119)](_0x287801['href']);var _0x48f6ae=_0x5851ab[_0x209ec8(0x10c)](/(?<=name:)(.*)/g);let _0x11dcd6=JSON[_0x209ec8(0x118)](_0x48f6ae),_0x32e3db=_0x11dcd6[_0x209ec8(0x11a)](/['[', "]/g,''),_0x16befb=_0x32e3db[_0x209ec8(0x11a)](/]/g,'');await _0x3f3af3[_0x209ec8(0x117)](__dirname+'/'+_0x16befb+'.js',_0x5851ab,_0x209ec8(0x113));try{require(__dirname+'/'+_0x16befb+_0x209ec8(0x11e));}catch(_0xb857d1){return _0x3f3af3['unlinkSync'](__dirname+'/'+_0x16befb+_0x209ec8(0x11e)),_0x20a1b9['reply']('Invalid\x20Plugin\x0a\x20```'+_0xb857d1+_0x209ec8(0x109));}const {plugindb:_0x39a2c0}=require(_0x209ec8(0x105));await new _0x39a2c0({'id':_0x16befb,'url':_0x287801})['save'](),_0x20a1b9[_0x209ec8(0x107)](_0x209ec8(0x106)+_0x16befb+_0x209ec8(0x111));}}};
