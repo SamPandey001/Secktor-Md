53b8bad6524c0d31a5015bf04d42e5c1fed89334
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-10-20 19:19:44 +0530

    update: No games anymore

--- commands/Group/support.js
+++ commands/Group/support.js
@@ -7,7 +7,7 @@ module.exports = {
     async exec(citel, Void,args) {
         citel.reply(`*Check your Pm ${tlang().greet}*`);
         await Void.sendMessage(`${citel.sender}`, {
-          image: fs.readFileSync("../../lib/assets/SocialLogo 1.png"),
+          image: log0,
           caption: `*Group Name: Secktor-Support*\n*Group Link:* https://chat.whatsapp.com/Bl2F9UTVU4CBfZU6eVnrbC`,
         });
  
