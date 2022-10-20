dbda2f73b36f0e40217030085932b108a1579e27
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-10-20 18:01:10 +0530

    Update kick.js

--- commands/Group/kick.js
+++ commands/Group/kick.js
@@ -5,18 +5,16 @@ module.exports = {
     category: 'group',
     desc: 'Kicks replied/quoted user from group.',
     async exec(citel, Void,args) {	    
-		const mentionByTag = citel.mtype == "extendedTextMessage" && citel.message.extendedTextMessage.contextInfo != null ? citel.message.extendedTextMessage.contextInfo.mentionedJid : [];
 			  if (!citel.isGroup) return citel.reply(tlang().group);
 			  const groupAdmins = await getAdmin(Void,citel)
 			  const botNumber =  await Void.decodeJid(Void.user.id) 
 			  const isBotAdmins = citel.isGroup ? groupAdmins.includes(botNumber) : false;
 			  const isAdmins = citel.isGroup ? groupAdmins.includes(citel.sender) : false;
 			  
-		  if (!isAdmins) return citel.reply(tlang().admin);
+	      if (!isAdmins) return citel.reply(tlang().admin);
 	      if (!isBotAdmins) return citel.reply(tlang().botAdmin);
 			  try {
-				let mention = mentionByTag;
-				let users = (await mention[0]) || citel.msg.contextInfo.participant;
+				let users = citel.mentionedJid[0] ? citel.mentionedJid[0] : citel.quoted ? citel.quoted.sender : args.join(" ").replace(/[^0-9]/g, "") + "@s.whatsapp.net";
 				if (!users) return;
 				await Void.groupParticipantsUpdate(citel.chat, [users], "remove");
 			  } catch  {
