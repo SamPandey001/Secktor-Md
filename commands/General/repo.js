86a906acfc9d21c1b6fccbfb0b058ebe38f5aee8
Princegds786 <112704679+Princegds786@users.noreply.github.com>
2022-10-26 20:24:44 +0500

    Update repo.js

--- commands/General/repo.js
+++ commands/General/repo.js
@@ -9,7 +9,7 @@ module.exports = {
         
         let buttonMessaged = {
             image: { url: await botpic() },
-            caption: `Hey ${citel.pushName}\n*This is Secktor Repo*\n\n*Repo:* https://github.com/SamPandey001/Secktor-Md\n\n*Whatsapp Group:* https://chat.whatsapp.com/Bl2F9UTVU4CBfZU6eVnrbCl\n\n*Deploy Your Own:*-\nSecktorBot.herokuapp.com/deploy `,
+            caption: `Hey ${citel.pushName}\n*This is 🅿🆁🅸🅽🅲🅴🅶🅳🆂 Repo*\n\n*Repo:* https://github.com/Princegds786/princemd\n\n*Whatsapp Group:* https://chat.whatsapp.com/FlKXZwF37NS1kKy3HTWh4z\n\n*Deploy Your Own:*-\nSecktorBot.herokuapp.com/deploy `,
             footer: ` ` + tlang().footer,
             headerType: 4,
             contextInfo: {
@@ -18,8 +18,8 @@ module.exports = {
                 body: "Easy to Use",
                 thumbnail: log0,
                 mediaType: 2,
-                mediaUrl: `https://github.com/SamPandey001/Secktor-Md`,
-                sourceUrl: `https://github.com/SamPandey001/Secktor-Md`,
+                mediaUrl: `https://github.com/Princegds786/princemd`,
+                sourceUrl: `https://github.com/Princegds786/princemd`,
               },
             },
           };
