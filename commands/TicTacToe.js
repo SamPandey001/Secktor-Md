7df18324f7d5bb95c70c9cf297159796651d23e7
mimibotto2 <115569091+mimibotto2@users.noreply.github.com>
2022-11-12 14:30:14 +0200

    Update TicTacToe.js

--- commands/TicTacToe.js
+++ commands/TicTacToe.js
@@ -3,9 +3,9 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : @samapndey002 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
+ * @project_name : Kusuo
+ * @author : Nocturno <https://github.com/mimibotto2>
+ * @description : 🍃Kusuo🥢,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
 const { tlang } = require('../lib')
