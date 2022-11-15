f184c5b710277257b4d9da1745afcdfe322c418b
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-11-15 12:04:16 +0530

    Update system.js

--- commands/system.js
+++ commands/system.js
@@ -119,9 +119,7 @@ cmd({
         async(Void, citel, text,{ isCreator }) => {
             if (!isCreator) return citel.reply("This command is onlt for my Owner")
             try {
-
-                let mention = citel.mentionedJid ? citel.mentionedJid[0] : citel.msg.contextInfo.participant || false;
-                let users = await (mention[0]) || citel.msg.contextInfo.participant
+                let users = citel.mentionedJid ? citel.mentionedJid[0] : citel.msg.contextInfo.participant || false;
                 if (!users) return citel.reply("Please mention any user.❌")
                 let pushnamer = Void.getName(users);
                 sck1.findOne({ id: users }).then(async(usr) => {
@@ -130,10 +128,9 @@ cmd({
                         return citel.reply(`${pushnamer} is unbanned.`)
                     } else {
                         console.log(usr.ban)
-                        if (usr.ban !== "true") return citel.reply(`${pushnamer} is already unbanned.`)
+                        if (usr.ban !== "true") return citel.reply(`${usr.name} is already unbanned.`)
                         await sck1.updateOne({ id: users }, { ban: "false" })
-                        console.log("user banned")
-                        return citel.reply(`${pushnamer} is free as a bird now`)
+                        return citel.reply(`${usr.name} is free as a bird now`)
                     }
                 })
             } catch {
@@ -253,22 +250,20 @@ cmd({
             category: "owner",
             desc: "Bans user from using bot."
         },
-        async(Void, citel, text, isCreator) => {
+        async(Void, citel, text,{ isCreator}) => {
             if (!isCreator) return citel.reply(tlang().owner)
-            const mentionByTag = citel.mtype == "extendedTextMessage" && citel.message.extendedTextMessage.contextInfo != null ? citel.message.extendedTextMessage.contextInfo.mentionedJid : [];
             try {
-                let mention = mentionByTag
-                let users = await (mention[0]) || citel.msg.contextInfo.participant
+                let users = citel.mentionedJid ? citel.mentionedJid[0] : citel.msg.contextInfo.participant || false;
                 if (!users) return citel.reply(`❌ Please mention any user ${tlang().greet}.`)
                 let pushnamer = Void.getName(users);
                 sck1.findOne({ id: users }).then(async(usr) => {
                     if (!usr) {
                         await new sck1({ id: users, ban: "true" }).save()
-                        return citel.reply(`Banned ${pushnamer} from Using Commands`)
+                        return citel.reply(`_Banned ${usr.name} from Using Commands._`)
                     } else {
                         if (usr.ban == "true") return citel.reply(`${pushnamer} is already Banned from Using Commands`)
                         await sck1.updateOne({ id: users }, { ban: "true" })
-                        return citel.reply(`Successfully Banned ${pushnamer} from Using Commands`)
+                        return citel.reply(`_Successfully Banned ${usr.name} from Using Commands._`)
                     }
                 })
             } catch (e) {
