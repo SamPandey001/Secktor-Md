034aa383255a96e89913fb03a8d31df9a7ae8219
TheSpeedY <104565822+SamPandey001@users.noreply.github.com>
2022-11-21 10:25:47 +0530

    Update _pm2.js

--- commands/_pm2.js
+++ commands/_pm2.js
@@ -17,9 +17,10 @@ cmd({
         category: "tools",
         filename: __filename
     },
-    async(Void, citel,{isCreator}) => {
+    async(Void, citel,text,{ isCreator }) => {
    if (!isCreator) return citel.reply(tlang().owner)
             const { exec } = require("child_process")
+            citel.reply('Restarting')
             exec('pm2 restart all')
     }
 );
