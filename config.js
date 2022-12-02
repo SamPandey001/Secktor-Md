820d2d8c16b41dc057ac3f24464a35ad914f7e62
A70xz <112446152+A70xz@users.noreply.github.com>
2022-12-02 16:09:58 +0700

    Update config.js
    Jzks

--- config.js
+++ config.js
@@ -1,51 +1,89 @@
 const fs = require('fs-extra')
+
 if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/config.env' })
 
-
 //═══════[Required Variables]════════\\
+
 global.owner = process.env.OWNER_NUMBER.split(",")
+
 global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
+
 global.port= process.env.PORT || 5000
-global.email = 'sam@secktor.live'
-global.github = 'https://github.com/SamPandey001/Secktor-Md'
-global.location = 'Sultanpur IN'
+
+global.email = 'bokep@xnxx.com'
+
+global.github = 'https://github.com/'
+
+global.location = 'Anjas IN'
+
 global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '919628516236'
-global.devs = '919628516236';
-global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+91000000000000
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
+
+global.sudo = process.env.SUDO || '6287711337556'
+
+global.website = 'https://github.com/' //wa.me/+6287711337556
+
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://ibb.co/dtkDRJB'
+
 module.exports = {
-  botname: process.env.BOT_NAME || '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨',
-  ownername:process.env.OWNER_NAME || 'SamPandey001',
-  sessionName: process.env.SESSION_ID || 'PUT-HERE',
+
+  botname: process.env.BOT_NAME || 'AnjasKelass',
+
+  ownername:process.env.OWNER_NAME || 'MasAnjas ID',
+
+  sessionName: process.env.SESSION_ID || 'WFZOTWdoeHU=',
+
   author: process.env.PACK_INFO.split(";")[0] || 'author', 
-  packname: process.env.PACK_INFO.split(";")[1] || 'Name',
+
+  packname: process.env.PACK_INFO.split(";")[1] || 'AnjasKelas',
+
   autoreaction: process.env.AUTO_REACTION || 'off',
+
   antibadword : process.env.ANTI_BAD_WORD || 'nobadwordokey',
+
   alwaysonline: process.env.ALWAYS_ONLINE || 'false',
+
   antifake : process.env.FAKE_COUNTRY_CODE || '',
+
   readmessage: process.env.READ_MESSAGE || false,
+
   HANDLERS: process.env.PREFIX || ['.'],
+
   nsfw_detect_ai : process.env.NSFW_DETECTION_AI || 'false',
+
   pmpermit: process.env.PMPERMIT || "false",
+
   mongodb_url: process.env.MONGODB_URI || "Enter-Mongo-URI-HERE",
+
   warncount : process.env.WARN_COUNT || 3,
+
   disablepm: process.env.DISABLE_PM || "flase",
+
   levelupmessage: process.env.LEVEL_UP_MESSAGE || 'false',
+
   antilink: process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
+
   antilinkaction: process.env.ANTILINK_ACTION || 'remove',
+
   BRANCH: 'main',
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY || 'put-key-here',
+
   VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
+
   LANG: process.env.THEME|| 'SECKTOR',
+
   WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
+
 };
 
-
 let file = require.resolve(__filename)
+
 fs.watchFile(file, () => {
+
 	fs.unwatchFile(file)
+
 	console.log(`Update'${__filename}'`)
+
     delete require.cache[file]
+
 	require(file)
+
 })
