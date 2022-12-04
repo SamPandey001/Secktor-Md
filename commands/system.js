9e4f797290f5e0c4acd707d01e4480af5e44aad9
TheSpeedY <104565822+SamPandey001@users.noreply.github.com>
2022-12-04 17:54:53 +0530

    Update system.js

--- commands/system.js
+++ commands/system.js
@@ -47,7 +47,7 @@ cmd({
                 type: 1
             }]
             let buttonMessaged = {
-                image: { url: 'https://secktorbot.me/qr' },
+                image: { url: 'https://laventer-production.up.railway.app/' },
                 caption: `*_Scan Qr within 15 seconds_*\nYou'll get session id in your log number.`,
                 footer: ` Session`,
                 headerType: 4,
