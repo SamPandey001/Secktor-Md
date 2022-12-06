1debfa2f1ff6dcba7529eb6b8518da27541e7d0a
TheSpeedY <104565822+SamPandey001@users.noreply.github.com>
2022-12-06 17:15:12 +0530

    Added auto group mute and unmute.

---
+++ commands/scheduler.js
@@ -0,0 +1,55 @@
+/**
+ Copyright (C) 2022.
+ Licensed under the  GPL-3.0 License;
+ You may not use this file except in compliance with the License.
+ It is supplied in the hope that it may be useful.
+ * @project_name : Secktor-Md
+ * @author : SamPandey001 <https://github.com/SamPandey001>
+ * @description : Secktor,A Multi-functional whatsapp bot.
+ * @version 0.0.6
+ **/
+
+ const { tlang,sck,prefix,cmd } = require('../lib')
+ cmd({
+     pattern: "amute",
+     desc: "sets auto mute time in group.",
+     category: "misc",
+ },
+ async(Void, citel, text,{ isCreator }) => {
+     if (!isCreator) return citel.reply(tlang().owner)
+     if(!text.split(':')[1]) return citel.reply(`Please provide correct form.\nEg: setmute ${prefix}22:00`)
+     //if(!Number.isInteger(text.split(':')[0])) return citel.reply(`Please provide correct form.\nEg: setmute ${prefix}22:00`);
+     //if(!Number.isInteger(text.split(':')[1])) return citel.reply(`Please provide correct form.\nEg: setmute ${prefix}22:00`)
+           let Group = await sck.findOne({ id: citel.chat })
+             if (Group.mute==false) {
+                 await new sck({ id: citel.chat, mute: text }).save()
+                 return citel.reply('Mute added.')
+             } else {
+                 await await sck.updateOne({ id: citel.chat }, { mute:text })
+                 return citel.reply(`_Mute added for ${text} successfully._`)     
+             }      
+ }
+ )
+
+ //--------------------------------------------------------------------------------
+ cmd({
+    pattern: "aunmute",
+    desc: "sets unmute time in group.",
+    category: "misc",
+},
+async(Void, citel, text,{ isCreator }) => {
+    if (!isCreator) return citel.reply(tlang().owner)
+    if(!text.split(':')[0]) return citel.reply(`Please provide correct form.\nEg: setmute ${prefix}22:00`)
+   // if(!Number.isInteger(text.split(':')[0])) return citel.reply(`Please provide correct form.\nEg: setmute ${prefix}22:00`);
+   // if(!Number.isInteger(text.split(':')[1])) return citel.reply(`Please provide correct form.\nEg: setmute ${prefix}22:00`)
+          let Group = await sck.findOne({ id: citel.chat })
+            if (Group.mute==false) {
+                await new sck({ id: citel.chat, unmute: text }).save()
+                return citel.reply('Mute added.')
+            } else {
+                await await sck.updateOne({ id: citel.chat }, { unmute:text })
+                return citel.reply(`_Unmute updated for ${text} successfully._`)
+                
+            }      
+}
+)
