b076ebbcac9ed3e79828ddd96ada2c3f0dccd667
TheSpeedY <104565822+SamPandey001@users.noreply.github.com>
2022-12-10 17:08:58 +0530

    Update koyeb.js

--- commands/koyeb.js
+++ commands/koyeb.js
@@ -10,7 +10,7 @@
  **/
 const axios = require('axios');
 const { tlang,cmd } = require('../lib')
-const { redeploy , getvar , delvar , getallvar , change_env} = require('../lib/koyeb')
+const { redeploy , getvar , delvar , getallvar , change_env , get_deployments} = require('../lib/koyeb')
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 cmd(
@@ -22,6 +22,8 @@ cmd(
   },
   async (Void,citel,text,{isCreator}) => {
        if(!isCreator) return citel.reply(tlang().owner);
+       let check = await get_deployments()
+       if(check==='true') return citel.reply('_Please wait..._\n_Currently 2 instances are running in Koyeb,wait to stop one of them._')
        let data = await redeploy();
        return citel.reply(data)
   })
@@ -63,6 +65,8 @@ cmd(
   async (Void,citel,text,{isCreator}) => {
        if(!isCreator) return citel.reply(tlang().owner);
        if(!text.split(':')[1]) return citel.reply('*Wrong Format.*\nPlease provide key and value.\n_Eg: .setvar THEME:SECKTOR_')
+       let check = await get_deployments()
+       if(check==='true') return citel.reply('_Please wait..._\n_Currently 2 instances are running in Koyeb,wait to stop one of them._')
        let data = await change_env(text)
        return citel.reply(data)
   })
@@ -78,6 +82,8 @@ cmd(
   async (Void,citel,text,{isCreator}) => {
        if(!isCreator) return citel.reply(tlang().owner);
        if(!text) return citel.reply('Please provide key.\n_Eg: .delvar PORT_')
+       let check = await get_deployments()
+       if(check==='true') return citel.reply('_Please wait..._\n_Currently 2 instances are running in Koyeb,wait to stop one of them._')
        let data = await delvar(text)
        return citel.reply(data)
   })
