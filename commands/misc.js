4e907e248c3cb20c7eeb9a138a47f480b68b3361
TheSpeedY <104565822+SamPandey001@users.noreply.github.com>
2022-12-16 19:21:21 +0530

    Steal fixed.

--- commands/misc.js
+++ commands/misc.js
@@ -11,6 +11,7 @@
 
  const { tlang, getAdmin, prefix, Config, sck, fetchJson, runtime,cmd } = require('../lib')
  let { dBinary, eBinary } = require("../lib/binary");
+const { Sticker, createSticker, StickerTypes } = require("wa-sticker-formatter");
  const fs = require('fs')
  const axios = require('axios')
   //---------------------------------------------------------------------------
