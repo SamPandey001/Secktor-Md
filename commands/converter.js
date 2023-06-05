d1ab6e69f0ae6e48d1949cc6990cdebf2bf107e3
DarkWinzo <darkwinzo4440@gmail.com>
2023-06-05 09:42:39 +0530

    Update converter.js

--- commands/converter.js
+++ commands/converter.js
@@ -104,9 +104,9 @@ cmd({
         async(Void, citel, text) => {
             if (isNaN(text.split(" ")[0]) || !text) {
                 let text = tiny(
-                    "Fancy text generator\n\nExample: .fancy 32 Secktor\n\n"
+                    "Fancy text generator\n\nExample: .fancy 32 Dark Shadow\n\n"
                 );
-                listall("Secktor Bot").forEach((txt, num) => {
+                listall("Dark Shadow").forEach((txt, num) => {
                     text += `${(num += 1)} ${txt}\n`;
                 });
                 return await citel.reply(text);
