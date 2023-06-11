40ed2d8b2c2134bc7e4ef652380b31f389a98d2a
TheSpeedY <104565822+SamPandey001@users.noreply.github.com>
2023-06-11 15:19:07 +0530

    Ping

--- commands/ping.js
+++ commands/ping.js
@@ -19,8 +19,8 @@ Secktor.cmd({
     },
     async(Void, citel) => {
         var inital = new Date().getTime();
-        await citel.reply('```Ping!!!```');
+        const { key } = await Void.sendMessage(citel.chat, {text: '```Ping!!!```'});
         var final = new Date().getTime();
-        return await citel.reply('*Pong*\n *' + (final - inital) + ' ms* ');
+       return await Void.sendMessage(citel.chat, {text: '*Pong*\n *' + (final - inital) + ' ms* ', edit: key});
     }
 );
