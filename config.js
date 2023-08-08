4bef25b491b3872849361c0ca4a0e27b972359ab
goku00799 <141681202+goku00799@users.noreply.github.com>
2023-08-09 00:47:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.c
 module.exports = {
   botname:   process.env.BOT_NAME === undefined ? 'ANVI' : process.env.BOT_NAME,
   ownername: process.env.OWNER_NAME === undefined ? GOKUBLACK' : process.env.OWNER_NAME,
-  sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
+  sessionName:  process.env.SESSION_ID === undefined ? Secktor;;;SupposeFlame : process.env.SESSION_ID,
   author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'SamPandey001' : process.env.PACK_INFO.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
   packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'Secktor-Md' : process.env.PACK_INFO.split(";")[1],
