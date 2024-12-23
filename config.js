8a95fb1fa78a7ca594af91a1394174d82cd5173c
AstroX10 <danielfx0012@gmail.com>
2024-12-23 19:04:06 +0100

    chore: breaking chnages
    Modern fixes, updates, just getting started

--- config.js
+++ config.js
@@ -1,58 +1,62 @@
-const fs = require('fs-extra')
-if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/config.env' })
-
+const fs = require('fs-extra');
+if (fs.existsSync('config.env'))
+	require('dotenv').config({ path: __dirname + '/config.env' });
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER.split(",")
-global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority"
-global.port= process.env.PORT || 5000
-global.email = 'sam@secktor.live'
-global.github = 'https://github.com/SamPandey001/Secktor-Md'
-global.location = 'Sultanpur IN'
-global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '919628516236'
+global.owner = process.env.OWNER_NUMBER?.split(',');
+global.mongodb =
+	process.env.MONGODB_URI ||
+	'mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority';
+global.port = process.env.PORT || 5000;
+global.email = 'sam@secktor.live';
+global.github = 'https://github.com/SamPandey001/Secktor-Md';
+global.location = 'Sultanpur IN';
+global.gurl = 'https://instagram.com/'; // add your username
+global.sudo = process.env.SUDO || '919628516236';
 global.devs = '919628516236';
-global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+91000000000000
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
+global.website = 'https://github.com/SamPandey001/Secktor-Md';
+global.THUMB_IMAGE =
+	process.env.THUMB_IMAGE ||
+	'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png';
+
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'SamPandey001' : process.env.OWNER_NAME,
-  sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
-  author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'SamPandey001' : process.env.PACK_INFO.split(";")[0],
-  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
-  packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'Secktor-Md' : process.env.PACK_INFO.split(";")[1],
-  autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
-  antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
-  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
-  antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '971' : process.env.FAKE_COUNTRY_CODE,
-  readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
-  auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
-  HANDLERS:  process.env.PREFIX === undefined ? ['.'] : process.env.PREFIX,
-  warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT,
-  disablepm:  process.env.DISABLE_PM === undefined ? false : process.env.DISABLE_PM,
-  levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? false : process.env.LEVEL_UP_MESSAGE,
-  antilink:  process.env.ANTILINK_VALUES === undefined ? 'chat.whatsapp.com' : process.env.ANTILINK_VALUES,
-  antilinkaction: process.env.ANTILINK_ACTION === undefined ? 'remove' : process.env.ANTILINK_ACTION,
-  BRANCH: 'main', 
-  ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? '' : process.env.ALIVE_MESSAGE,
-  autobio:  process.env.AUTO_BIO === undefined ? false : process.env.AUTO_BIO,
-  OPENAI_API_KEY:  process.env.OPENAI_API_KEY === undefined ? false : process.env.OPENAI_API_KEY,
-  heroku:  process.env.heroku === undefined ? false : process.env.heroku,
-  HEROKU: {
-    HEROKU: process.env.HEROKU ||false,
-    API_KEY: process.env.HEROKU_API_KEY === undefined ? '1abfce1e-1bee-4334-9f6c-f4c1cb1cafab' : process.env.HEROKU_API_KEY,
-    APP_NAME: process.env.HEROKU_APP_NAME === undefined ? 'zeropgg' : process.env.HEROKU_APP_NAME
-},
-  VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
-  LANG: process.env.THEME|| 'SECKTOR',
-  WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
+	botname: process.env.BOT_NAME || '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨',
+	ownername: process.env.OWNER_NAME || 'SamPandey001',
+	sessionName: process.env.SESSION_ID || false,
+	author: (process.env.PACK_INFO?.split(';') || [])[0] || 'SamPandey001',
+	auto_read_status: process.env.AUTO_READ_STATUS || false,
+	packname: (process.env.PACK_INFO?.split(';') || [])[1] || 'Secktor-Md',
+	autoreaction: process.env.AUTO_REACTION || false,
+	antibadword: process.env.ANTI_BAD_WORD || null,
+	alwaysonline: process.env.ALWAYS_ONLINE || false,
+	antifake: process.env.FAKE_COUNTRY_CODE || '1000',
+	readmessage: process.env.READ_MESSAGE || false,
+	auto_status_saver: process.env.AUTO_STATUS_SAVER || false,
+	HANDLERS: process.env.PREFIX || ['.'],
+	warncount: process.env.WARN_COUNT || 3,
+	disablepm: process.env.DISABLE_PM || false,
+	levelupmessage: process.env.LEVEL_UP_MESSAGE || false,
+	antilink: process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
+	antilinkaction: process.env.ANTILINK_ACTION || 'remove',
+	BRANCH: 'main',
+	ALIVE_MESSAGE: process.env.ALIVE_MESSAGE || '',
+	autobio: process.env.AUTO_BIO || false,
+	OPENAI_API_KEY: process.env.OPENAI_API_KEY || false,
+	heroku: process.env.HEROKU || false,
+	HEROKU: {
+		HEROKU: process.env.HEROKU || false,
+		API_KEY: process.env.HEROKU_API_KEY || '',
+		APP_NAME: process.env.HEROKU_APP_NAME || '',
+	},
+	VERSION: process.env.VERSION || 'v.0.0.4',
+	LANG: process.env.THEME || 'SECKTOR',
+	WORKTYPE: process.env.WORKTYPE || 'public',
 };
 
-
-let file = require.resolve(__filename)
+let file = require.resolve(__filename);
 fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(`Update'${__filename}'`)
-    delete require.cache[file]
-	require(file)
-})
+	fs.unwatchFile(file);
+	console.log(`Update '${__filename}'`);
+	delete require.cache[file];
+	require(file);
+});
