dfcc65cd0b899607727ec14eb8f8caf90bfff561
AstroX10 <danielfx0012@gmail.com>
2024-12-23 21:30:58 +0100

    feat: fixed version inconsistence

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ module.exports = {
 		API_KEY: process.env.HEROKU_API_KEY || '',
 		APP_NAME: process.env.HEROKU_APP_NAME || '',
 	},
-	VERSION: process.env.VERSION || 'v.0.0.4',
+	VERSION: process.env.VERSION || 'v.0.0.8',
 	LANG: process.env.THEME || 'SECKTOR',
 	WORKTYPE: process.env.WORKTYPE || 'public',
 };
