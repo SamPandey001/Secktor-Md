a95eb90a04e0eaff66c6c0e02828cc7a676b73fd
AstroX10 <danielfx0012@gmail.com>
2024-12-24 11:58:18 +0100

    Update switch-act.js

--- commands/switch-act.js
+++ commands/switch-act.js
@@ -1,113 +1,94 @@
 /**
- Copyright (C) 2022.
- Licensed under the  GPL-3.0 License;
- You may not use this file except in compliance with the License.
- It is supplied in the hope that it may be useful.
+ * Copyright (C) 2022.
+ * Licensed under the GPL-3.0 License;
+ * You may not use this file except in compliance with the License.
+ * It is supplied in the hope that it may be useful.
  * @project_name : Secktor-Md
  * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
- * @version 0.0.6
+ * @description : Secktor, A Multi-functional WhatsApp bot.
+ * @version 0.0.8
  **/
 
-const { cmd,sck,sck1, getAdmin, tlang, prefix } = require('../lib')
-const Config = require('../config')
-    //---------------------------------------------------------------------------
+const { cmd, sck, getAdmin, tlang } = require('../lib');
+
 cmd({
-        pattern: "act",
-        desc: "Switches for varios works.",
-        category: "group",
-        filename: __filename,
-    },
-    async(Void, citel, text,{ isCreator }) => {
-        //-----------------------------------------
-        if (!citel.isGroup) return citel.reply(tlang().group);
-        const groupAdmins = await getAdmin(Void, citel)
-        const botNumber = await Void.decodeJid(Void.user.id)
-        const isBotAdmins = citel.isGroup ? groupAdmins.includes(botNumber) : false;
-        const isAdmins = citel.isGroup ? groupAdmins.includes(citel.sender) : false;
-        //-----------------------------------------
-        if (!citel.isGroup) return citel.reply("This command is only for group")
-        if (!text) return citel.reply(`❌ Please provide me term like like\n1-events\n2-antilink\n3-nsfw\n4-cardgame\n5-bot`)
-        if (!isAdmins) return citel.reply("❌ This command is only for admin")
-        switch (text.split(" ")[0]) {
-            case 'antilink':
-                {
-                    let checkgroup = await sck.findOne({ id: citel.chat })
-                    if (!checkgroup) {
-                        await new sck({ id: citel.chat, antilink: "true" })
-                            .save()
-                        return citel.reply(' Antilink Enabled Successfully')
-                    } else {
-                        if (checkgroup.antilink == "true") return citel.reply("Antilink was alredy  enabled here.")
-                        await sck.updateOne({ id: citel.chat }, { antilink: "true" })
-                        citel.reply('Enabled antilink in current chat.')
-                        return
-                    }
-                }
-                break
-          
-                      case 'economy':
-                {
-                    let checkgroup = await sck.findOne({ id: citel.chat })
-                    if (!checkgroup) {
-                        await new sck({ id: citel.chat, economy: "true" })
-                            .save()
-                        return citel.reply(' Economy Enabled Successfully')
-                    } else {
-                        if (checkgroup.economy == "true") return citel.reply("Economy was alredy enabled.")
-                        await sck.updateOne({ id: citel.chat }, { economy: "true" })
-                        citel.reply('Economy enabled in current chat.')
-                        return
-                    }
-                }
-                break
-            case 'events':
-                {
-                    let checkgroup = await sck.findOne({ id: citel.chat })
-                    if (!checkgroup) {
-                        await new sck({ id: citel.chat, events: "true" })
-                            .save()
-                        return citel.reply("Successfully Enabled *Events*")
-                    } else {
-                        if (checkgroup.events == "true") return citel.reply("*Events* are already enabled")
-                        await sck.updateOne({ id: citel.chat }, { events: "true" })
-                        return citel.reply("Successfully Enabled *Events*")
-                    }
-                }
-                break
-            case 'cardgame':
-                {
-                    let checkgroup = sck.findOne({ id: citel.chat })
-                    if (!checkgroup) {
-                        await new sck({ id: citel.chat, cardgame: "active" })
-                            .save()
-                        return citel.reply("Successfully Enabled *Card Game*")
-                    } else {
-                        if (checkgroup.cardgame == "active") return citel.reply("*Card Game* was already enabled")
-                        await sck.updateOne({ id: citel.chat }, { cardgame: "active" })
-                        return citel.reply("Successfully Enabled *Card Game.*")
-                    }
-                }
-                break
-            case 'nsfw':
-                {
-                    let checkgroup = await sck.findOne({ id: citel.chat })
-                    if (!checkgroup) {
-                        await new sck({ id: citel.chat, nsfw: "true" })
-                            .save()
-                        return citel.reply("Successfully Enabled *NSFW*")
-                    } else {
-                        if (checkgroup.nsfw == "true") return citel.reply("*NSFW* is already enabled")
-                        await sck.updateOne({ id: citel.chat }, { nsfw: "true" })
-                        citel.reply("Successfully Enabled *NSFW*")
-                        return
-                    }
-                }
-                break
-            default:
-                {
-                    citel.reply("Please provide me term like.\n1-events\n2-antilink\n3-nsfw\n4-economy")
-                }
-        }
+    pattern: "act",
+    desc: "Switches for various works.",
+    category: "group",
+    filename: __filename,
+}, async (Void, citel, text) => {
+    if (!citel.isGroup) return citel.reply(tlang().group);
+
+    const groupAdmins = await getAdmin(Void, citel);
+    const isAdmins = citel.isGroup ? groupAdmins.includes(citel.sender) : false;
+
+    if (!isAdmins) return citel.reply("❌ This command is only for admins.");
+    if (!text) {
+        return citel.reply(`❌ Please provide me with a term like:\n1- events\n2- antilink\n3- nsfw\n4- economy\n5- cardgame\n6- bot`);
     }
-)
+
+    switch (text.split(" ")[0]) {
+        case 'antilink':
+            {
+                let checkgroup = await sck.findOne({ id: citel.chat });
+                if (!checkgroup) {
+                    await new sck({ id: citel.chat, antilink: "true" }).save();
+                    return citel.reply('Antilink enabled successfully.');
+                } else if (checkgroup.antilink === "true") {
+                    return citel.reply("Antilink is already enabled here.");
+                }
+                await sck.updateOne({ id: citel.chat }, { antilink: "true" });
+                return citel.reply('Antilink enabled in the current chat.');
+            }
+        case 'economy':
+            {
+                let checkgroup = await sck.findOne({ id: citel.chat });
+                if (!checkgroup) {
+                    await new sck({ id: citel.chat, economy: "true" }).save();
+                    return citel.reply('Economy enabled successfully.');
+                } else if (checkgroup.economy === "true") {
+                    return citel.reply("Economy is already enabled.");
+                }
+                await sck.updateOne({ id: citel.chat }, { economy: "true" });
+                return citel.reply('Economy enabled in the current chat.');
+            }
+        case 'events':
+            {
+                let checkgroup = await sck.findOne({ id: citel.chat });
+                if (!checkgroup) {
+                    await new sck({ id: citel.chat, events: "true" }).save();
+                    return citel.reply('Successfully enabled *Events*.');
+                } else if (checkgroup.events === "true") {
+                    return citel.reply('*Events* are already enabled.');
+                }
+                await sck.updateOne({ id: citel.chat }, { events: "true" });
+                return citel.reply('Successfully enabled *Events*.');
+            }
+        case 'cardgame':
+            {
+                let checkgroup = await sck.findOne({ id: citel.chat });
+                if (!checkgroup) {
+                    await new sck({ id: citel.chat, cardgame: "active" }).save();
+                    return citel.reply('Successfully enabled *Card Game*.');
+                } else if (checkgroup.cardgame === "active") {
+                    return citel.reply('*Card Game* is already enabled.');
+                }
+                await sck.updateOne({ id: citel.chat }, { cardgame: "active" });
+                return citel.reply('Successfully enabled *Card Game*.');
+            }
+        case 'nsfw':
+            {
+                let checkgroup = await sck.findOne({ id: citel.chat });
+                if (!checkgroup) {
+                    await new sck({ id: citel.chat, nsfw: "true" }).save();
+                    return citel.reply('Successfully enabled *NSFW*.');
+                } else if (checkgroup.nsfw === "true") {
+                    return citel.reply('*NSFW* is already enabled.');
+                }
+                await sck.updateOne({ id: citel.chat }, { nsfw: "true" });
+                return citel.reply('Successfully enabled *NSFW*.');
+            }
+        default:
+            citel.reply("❌ Please provide a valid option:\n1- events\n2- antilink\n3- nsfw\n4- economy\n5- cardgame");
+    }
+});
