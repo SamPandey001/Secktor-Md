4f1de1fb6e73e62500d03987609154f5e2bf6143
Siraj <166923994+BK9dev@users.noreply.github.com>
2024-12-27 15:34:37 +0100

    Update converter.js

--- commands/converter.js
+++ commands/converter.js
@@ -10,7 +10,7 @@
  **/
 
 const axios = require('axios')
-const { sck1, tiny, fancytext, listall,cmd,ffmpeg } = require('../lib/')
+const { sck1, tiny, fancytext, listall,cmd,ffmpeg, Config } = require('../lib/')
 const fs = require('fs-extra');
 const { exec } = require('child_process')
 const { Sticker, createSticker, StickerTypes } = require("wa-sticker-formatter");
