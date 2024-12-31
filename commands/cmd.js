46abbeb5583dd4d94ccca8b93d83ba4f939e9e87
TheSpeedY <104565822+SamPandey001@users.noreply.github.com>
2025-01-01 05:21:46 +0530

    test

--- commands/cmd.js
+++ commands/cmd.js
@@ -1,12 +1,14 @@
-const { cmd, sck, commands } = require(__dirname + '/../lib');
+const { cmd, sck, commands,tlang } = require(__dirname + '/../lib');
 
 cmd({
     pattern: "cmd",
-    desc: "Manage custom commands for specific group.",
-    category: "admin",
+    desc: "Give access of private commands in group.",
+    category: "owner",
 },
 async (Void, citel, text, { isCreator }) => {
-    if (!isCreator) return citel.reply("Only the owner can manage commands.");
+    if (!isCreator) return citel.reply(tlang().owner);
+    if (!citel.isGroup) return citel.reply(tlang().group);
+      
 
     const [action, commandName] = text.split(" ");
     let group = await sck.findOne({ id: citel.chat });
